456294240557b065735c15505ef3ae2d2fb2f797
Noanihal <121955492+Noanihal@users.noreply.github.com>
2026-04-27 15:40:37 +0530

    Update and rename PersonalGreeting.java to SimpleSubtraction.java

---
+++ SimpleSubtraction.java
@@ -0,0 +1,8 @@
+public class SimpleSubtraction {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 10;
+        int result = a - b;
+        System.out.println("Result: " + result); // Output: 15
+    }
+}
